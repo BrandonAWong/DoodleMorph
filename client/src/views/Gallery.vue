59b61b5b2c79fae8c61ea1231630c82459179e6a
Carlos Aguilera <18aguilerac@gmail.com>
2025-03-23 08:39:09 -0700

    update

--- client/src/views/Gallery.vue
+++ client/src/views/Gallery.vue
@@ -1,38 +1,56 @@
 <script setup lang="ts">
 import SmartSvg from '@/components/smart/SmartSvg.vue';
-import { inject, ref } from 'vue';
+import { inject, onMounted, ref, useTemplateRef, watch } from 'vue';
+
+interface Image {
+  creationdate: string;
+  doodle: string;
+  generated: string;
+  id: number;
+}
 
 const startOverlay = inject('start-overlay', () => {});
 const stopOverlay = inject('stop-overlay', () => {});
 
-let curPage = 0;
-let images = ref<any[]>([]);
+const refGallery = useTemplateRef('refGallery');
 
-window.onload = async () => {
-  await getImages();
-}
+const curPage = ref(0);
+const images = ref<Image[]>([]);
 
 async function getImages() {
   try {
     startOverlay();
 
-    const res = await fetch(`http://45.49.181.126:6521/image?limit=5&offset=${curPage++}`, {
+    const res = await fetch(`http://45.49.181.126:6521/image?limit=5&offset=${curPage.value * 5}`, {
       method: 'GET',
     });
 
     const data = await res.json();
 
-    const tImages = data.images;
-    images.value = [...images.value, ...data.images];
+    console.log(data);
+
+    images.value = data.images;
+
+    refGallery.value?.scrollIntoView({
+      behavior: 'smooth',
+    });
   } finally {
     stopOverlay();
   }
 }
+
+watch(curPage, () => {
+  getImages();
+});
+
+onMounted(() => {
+  getImages();
+});
 </script>
 
 <template>
   <main
-    class="relative h-screen bg-[#E5E6F3] bg-cover bg-center"
+    class="font-notebook relative min-h-screen bg-[#E5E6F3] bg-cover bg-center py-10 text-3xl font-black"
     :style="{ backgroundImage: 'url(/images/background.png)' }"
   >
     <!-- This is for the gallery icon in the top right which links to the gallery page. -->
@@ -51,6 +69,33 @@ async function getImages() {
       >
         Gallery
       </div>
+    </div>
+
+    <div ref="refGallery">
+      <figure v-for="image in images" :key="image.id" class="mx-auto flex w-3/4 gap-1">
+        <div class="mb-1 rounded border-4 border-[#394DA8]">
+          <img alt="doodle" :src="image.doodle" width="1080" height="720" />
+        </div>
+        <div class="mb-1 rounded border-4 border-[#394DA8]">
+          <img alt="generated" :src="image.generated" width="1080" height="720" />
+        </div>
+      </figure>
+    </div>
+
+    <div class="flex justify-center gap-2 pt-10 pb-15">
+      <button
+        class="btn border-[#394DA8] bg-[#E5E6F3]"
+        :class="{
+          'btn-disabled': curPage === 0,
+        }"
+        type="button"
+        @click="--curPage"
+      >
+        Prev Page
+      </button>
+      <button class="btn border-[#394DA8] bg-[#E5E6F3]" type="button" @click="++curPage">
+        Next Page
+      </button>
     </div>
   </main>
 </template>
